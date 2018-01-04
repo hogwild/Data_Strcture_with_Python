f7fb67554bacf40537114466c8aa5fd99d8d3ad7
hogwild <guxianbinmail@gmail.com>
2018-01-05 00:53:10 +0800

    with python files

---
+++ chapter3/LinkedList.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  4 23:21:13 2018
+
+@author: hogwild
+"""
+
+class LNode:
+    def __init__(self, elem, next_=None):
+        self.elem = elem
+        self.next = next_
